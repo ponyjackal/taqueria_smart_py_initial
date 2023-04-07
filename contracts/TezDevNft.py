f558ed4fe7416b8257c95ffd0f399c2e47149e44
ponyjackal <pony1125@outlook.com>
2023-04-07 10:41:06 -0700

    feat: deploy on ghostnet

--- contracts/TezDevNft.py
+++ contracts/TezDevNft.py
@@ -5,18 +5,21 @@ FA2 = sp.io.import_script_from_url(
 )
 
 class TezDevNFT(FA2.Fa2Nft):
-    @sp.entry_point
-    def mint(self, owner, token_info):
-        token_id = self.data.last_token_id
-        self.data.ledger[token_id] = owner
-        # self.data.token_metadata[token_id] = 
+    pass
 
 @sp.add_test(name = "Test tezDevNft")
 def test():
     sc = sp.test_scenario()
     tezDevNft = TezDevNFT(
         metadata = sp.utils.metadata_of_url(
-            " https://gateway.pinata.cloud/ipfs/QmRj2GC9evHerFyg8i8F7deu3D4UTuuUTcwmsiYwjLQsPD"
+            "https://gateway.pinata.cloud/ipfs/QmRj2GC9evHerFyg8i8F7deu3D4UTuuUTcwmsiYwjLQsPD"
         )
     )
     sc += tezDevNft
+
+sp.add_compilation_target("tezDevNFT", TezDevNFT(
+        metadata = sp.utils.metadata_of_url(
+            "https://gateway.pinata.cloud/ipfs/QmRj2GC9evHerFyg8i8F7deu3D4UTuuUTcwmsiYwjLQsPD"
+        )
+    )
+)
