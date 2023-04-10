346f6c5160ff45b657e46864b75c959062228218
ponyjackal <pony1125@outlook.com>
2023-04-10 07:55:12 -0700

    feat: update mint function

--- contracts/TezDevNft.py
+++ contracts/TezDevNft.py
@@ -3,9 +3,22 @@ import smartpy as sp
 FA2 = sp.io.import_script_from_url(
     "https://smartpy.io/templates/fa2_lib.py"
 )
+Utils = sp.io.import_script_from_url(
+    "https://raw.githubusercontent.com/RomarQ/tezos-sc-utils/main/smartpy/utils.py"
+)
 
 class TezDevNFT(FA2.Fa2Nft):
-    pass
+    @sp.entry_point
+    def mint(self, token_info):
+        sp.verify(sp.amount >= sp.tez(5),
+                      "INSUFFICIENT AMOUNT OF TEZOS")
+
+        token_id = self.data.last_token_id
+        self.data.token_metadata[token_id] = sp.record(
+            token_id=token_id, token_info=token_info
+        )
+        self.data.ledger[token_id] = sp.sender
+        self.data.last_token_id += 1
 
 @sp.add_test(name = "Test tezDevNft")
 def test():
